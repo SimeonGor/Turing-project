7760924d6b27e6c37ff15756e7e8b7b3b47b9cf4
Karina <thealisky@gmail.com>
2024-07-29 17:15:43 +0300

    Initialized clear project

---
+++ src/main/java/com/example/turing_project/TuringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.turing_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringProjectApplication.class, args);
+    }
+
+}
