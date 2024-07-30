3eac0c26482f77ea1dd2e2222484641b95eb73e9
simeongor <simeon.gor@yandex.ru>
2024-07-31 00:21:24 +0300

    feat: add method getEmployeeById

--- src/main/java/com/example/turing_project/repo/EmployeeRepo.java
+++ src/main/java/com/example/turing_project/repo/EmployeeRepo.java
@@ -3,5 +3,8 @@ package com.example.turing_project.repo;
 import com.example.turing_project.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public  interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    Optional<Employee> findById(Long id);
 }
--- src/main/java/com/example/turing_project/service/EmployeeService.java
+++ src/main/java/com/example/turing_project/service/EmployeeService.java
@@ -1,11 +1,19 @@
 package com.example.turing_project.service;
 
+import com.example.turing_project.entity.Employee;
 import com.example.turing_project.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
+
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> employeeOptional = employeeRepo.findById(id);
+        return employeeOptional.orElse(new Employee());
+    }
 }
