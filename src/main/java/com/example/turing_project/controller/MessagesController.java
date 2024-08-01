70e23528f2c3db896453476f22d1c8ac50d482b6
simeongor <simeon.gor@yandex.ru>
2024-08-01 17:59:12 +0300

    feat: update MessagesController to work with dialogs

--- src/main/java/com/example/turing_project/controller/MessagesController.java
+++ src/main/java/com/example/turing_project/controller/MessagesController.java
@@ -1,50 +1,62 @@
 package com.example.turing_project.controller;
 
-import com.example.turing_project.entity.Answer;
-import com.example.turing_project.entity.Message;
-import com.example.turing_project.entity.Question;
-import com.example.turing_project.service.EmployeeService;
+import com.example.turing_project.dto.AnswerDto;
+import com.example.turing_project.dto.DialogDto;
+import com.example.turing_project.dto.MessageDto;
+import com.example.turing_project.dto.QuestionDto;
 import com.example.turing_project.service.HistoryService;
 import com.example.turing_project.service.TuringService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+// TODO: 01.08.2024 необходима проверка принадлежности к пользователю
 @AllArgsConstructor
 @RestController
 @RequestMapping("/turing")
 public class MessagesController {
     private final HistoryService historyService;
     private final TuringService turingService;
-    private final EmployeeService employeeService;
 
-    @GetMapping("users/{userId}/messages/{messageId}")
-    public Message getMessage(@PathVariable Long userId, @PathVariable Long messageId) {
-        Message message = historyService.getMessageById(messageId);
+    @GetMapping("messages/{messageId}")
+    public MessageDto getMessage(@PathVariable Long messageId) {
+        MessageDto message = historyService.getMessageById(messageId);
         if (message == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No such message id");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such message id");
         }
         return message;
     }
 
-    @GetMapping("users/{userId}/messages")
-    public List<Message> getAllMessages(@PathVariable Long userId) {
-        return historyService.getAllUserMessages(userId);
+    @GetMapping("dialogs")
+    public List<DialogDto> getAllUserDialogs() {
+        // FIXME: 01.08.2024 необходим пользователь
+        return historyService.getAllDialogs(1L);
     }
 
-    // TODO: 30.07.2024 переделать определение пользователя
-    @PostMapping("users/{userId}/send")
-    public Answer sendQuestion(@PathVariable Long userId, @RequestBody Question question) {
-        Answer answer = turingService.handle(question.getText());
-        Message message = new Message();
-        message.setQuestion(question);
-        message.setAnswer(answer);
-        message.setEmployee(employeeService.getEmployeeById(userId));
+    @GetMapping("dialogs/{dialogId}/messages")
+    public List<MessageDto> getDialog(@PathVariable Long dialogId) {
+        List<MessageDto> messageDtoList = historyService.getDialogById(dialogId);
+        if (messageDtoList == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such dialog id");
+        }
+        return messageDtoList;
+    }
 
-        historyService.saveMessage(message);
+    @PostMapping("dialogs/new")
+    public DialogDto startNewDialog() {
+        return historyService.createDialog(1L, "New dialog %s".formatted(LocalDateTime.now().toString()));
+    }
+
+    @PostMapping("dialogs/{dialogId}/send")
+    public AnswerDto sendQuestion(@PathVariable Long dialogId, @RequestBody @Valid QuestionDto question) {
+        System.out.println(question.getText());
+        AnswerDto answer = turingService.handle(question.getText());
+        historyService.saveMessage(dialogId, question, answer);
 
         return answer;
     }
