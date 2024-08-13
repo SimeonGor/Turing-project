bfddc7ac86706510a2e86c414cbaa32b67034130
simeongor <simeon.gor@yandex.ru>
2024-08-14 01:52:24 +0300

    refactor: remove unused imports

--- src/main/java/com/example/turing_project/controller/DocumentController.java
+++ src/main/java/com/example/turing_project/controller/DocumentController.java
@@ -1,10 +1,12 @@
 package com.example.turing_project.controller;
 
 import com.example.turing_project.service.DocumentService;
-import com.example.turing_project.service.StorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
