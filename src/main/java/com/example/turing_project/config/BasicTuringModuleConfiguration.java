a4b16ff6d4296642a582faedb377399d3bcc2624
simeon.gor <simeon.gor@yandex.ru>
2024-09-04 19:05:54 +0300

    feat: remove unused imports

--- src/main/java/com/example/turing_project/config/BasicTuringModuleConfiguration.java
+++ src/main/java/com/example/turing_project/config/BasicTuringModuleConfiguration.java
@@ -1,14 +1,9 @@
 package com.example.turing_project.config;
 
-import com.example.turing_project.giga_chat.config.GigaChatProperties;
-import com.example.turing_project.llama3.config.LlamaProperties;
-import com.example.turing_project.llama3.service.LlamaInvoker;
 import com.example.turing_project.service.BasicTuringService;
-import com.example.turing_project.giga_chat.service.GigaChatInvoker;
 import com.example.turing_project.service.LLMInvoker;
 import com.example.turing_project.service.TuringService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,9 +15,4 @@ public class BasicTuringModuleConfiguration {
         llmInvoker.init();
         return new BasicTuringService(llmInvoker);
     }
-
-//    @Bean
-//    public LLMInvoker gigaChat(GigaChatProperties properties) {
-//        return new GigaChatInvoker(properties);
-//    }
 }
